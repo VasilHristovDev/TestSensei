d9ff026ab54356c620a4d96ce94ff91879575d0a
Vasil Hristov <35114175+VasilHristovDev@users.noreply.github.com>
2021-02-11 23:09:43 +0200

    Add files via upload

---
+++ dbh.php
@@ -0,0 +1,7 @@
+<?php
+    $dbServername="localhost";
+    $dbUsername ="root";
+    $dbPassword = "parola";
+    $dbName = "informatics test";
+
+    $conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName,3307);
